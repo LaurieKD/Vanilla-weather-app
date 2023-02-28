5415631ec2c675b1790c756ab244fc9d15694113
LaurieKD <124356856+LaurieKD@users.noreply.github.com>
2023-02-28 11:33:12 +0100

    Showing relevant weather icon

--- src/app.js
+++ src/app.js
@@ -21,19 +21,20 @@ function displayTemperature(response) {
 	let humidityElement = document.querySelector("#humidity");
 	let windElement = document.querySelector("#wind");
 	let dayElement = document.querySelector("#day");
+	let iconElement = document.querySelector("#icon");
 	temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 	cityElement.innerHTML = response.data.city;
 	descriptionElement.innerHTML = response.data.condition.description;
 	humidityElement.innerHTML = response.data.temperature.humidity;
 	windElement.innerHTML = response.data.wind.speed;
 	dayElement.innerHTML = formatDate(response.data.time * 1000);
+	iconElement.setAttribute("src", response.data.condition.icon_url);
+	iconElement.setAttribute("alt", response.data.condition.icon);
 }
 
 let city = "The Hague";
 let apiKey = "2335b3c82b10f0343t05f9bo28bfaca3";
 let apiUrlCity = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 let apiUrlCoord = `https://api.shecodes.io/weather/v1/current?lon={lon}&lat={lat}&key=${apiKey}&units=metric`;
-
-console.log(apiUrlCity);
 
 axios.get(apiUrlCity).then(displayTemperature);

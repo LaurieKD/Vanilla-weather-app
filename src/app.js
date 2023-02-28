9480d525dab5c0b6763784a9a2fce5dbc62bd50b
LaurieKD <124356856+LaurieKD@users.noreply.github.com>
2023-02-28 11:14:09 +0100

    Showing current day and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp) {
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10) {
+		minutes = `0${hours}`;
+	}
+	let minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+	let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	let day = days[date.getDay()];
+	return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
 	console.log(response.data);
 	let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +20,13 @@ function displayTemperature(response) {
 	let descriptionElement = document.querySelector("#description");
 	let humidityElement = document.querySelector("#humidity");
 	let windElement = document.querySelector("#wind");
+	let dayElement = document.querySelector("#day");
 	temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 	cityElement.innerHTML = response.data.city;
 	descriptionElement.innerHTML = response.data.condition.description;
 	humidityElement.innerHTML = response.data.temperature.humidity;
 	windElement.innerHTML = response.data.wind.speed;
+	dayElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let city = "The Hague";

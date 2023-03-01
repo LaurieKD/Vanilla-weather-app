b9a26e9a1f36b64563de987501ced045c10df3ee
LaurieKD <124356856+LaurieKD@users.noreply.github.com>
2023-03-01 16:45:58 +0100

    Integrated API in JS

--- src/app.js
+++ src/app.js
@@ -13,24 +13,35 @@ function formatDate(timestamp) {
 	return `${day}, ${hours}:${minutes}`;
 }
 
-function displayForecast(response) {
-	console.log(response.data.daily);
-	let forecastElement = document.querySelector("#forecast");
+function formatForecastDay(timestamp) {
+	let date = new Date(timestamp * 1000);
 
+	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+	let day = days[date.getDay()];
+	return day;
+}
+
+function displayForecast(response) {
+	let forecast = response.data.daily;
+	let forecastElement = document.querySelector("#forecast");
 	let forecastHTML = `<div class="row">`;
-	let days = ["Wed", "Thu", "Fri"];
-	days.forEach(function (day) {
-		forecastHTML =
-			forecastHTML +
-			` 
+
+	forecast.forEach(function (forecastDay, index) {
+		if (index < 6) {
+			forecastHTML =
+				forecastHTML +
+				` 
             <div class="col-2">
               <div class="weather-forecast-date" >
-              ${day}
+              ${formatForecastDay(forecastDay.time)}
               </div>
-              <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" alt="sunny" class="icon-forecast">
-              <div class="weather-forecast-temp"><span class="warm">18</span><span class="cold"> 12</span> </div>
+              <img src=${forecastDay.condition.icon_url} alt=${forecastDay.condition.icon}>
+              <div class="weather-forecast-temp"><span class="warm">${Math.round(
+								forecastDay.temperature.maximum
+							)}</span>°<span class="cold"> ${Math.round(forecastDay.temperature.minimum)}</span>° </div>
             </div>
 `;
+		}
 	});
 
 	forecastHTML = forecastHTML + `</div>`;
